a72db58a53ba5b7e154f11dea1c4847bc28723bd
Jessi Iler <inertial.observer@gmail.com>
2019-11-14 20:51:37 -0800

    add 'submit' button widget without action

--- venv/main.py
+++ venv/main.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 
 class MyGrid(GridLayout):
@@ -20,6 +21,9 @@ class MyGrid(GridLayout):
         self.add_widget(Label(text="Email: "))
         self.email = TextInput(multiline=False)
         self.add_widget(self.email)
+
+        self.submit = Button(text="Submit", font_size=40)
+        self.add_widget(self.submit)
 
 
 class MyApp(App):
