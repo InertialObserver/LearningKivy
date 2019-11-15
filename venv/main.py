354a3aa895169054e471d41734c2f73342acce72
Jessi Iler <inertial.observer@gmail.com>
2019-11-15 15:16:27 -0800

    button changes text when clicked

--- venv/main.py
+++ venv/main.py
@@ -1,6 +1,8 @@
 import kivy
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.uix.button import ButtonBehavior
 
 
 class MyApp(App):
